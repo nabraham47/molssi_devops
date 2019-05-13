583d57414a60c0702d0a05a5bc360eac45acb691
Nate Abraham <nate.abraham@colorado.edu>
2019-05-13 13:35:10 -0600

    added my own funciton for the mean of a list in molssi_math.py

--- molssi_devops/molssi_math.py
+++ molssi_devops/molssi_math.py
@@ -4,7 +4,6 @@ A package containing useful math functions
 
 Handles the primary functions
 """
-#test this
 
 def canvas(with_attribution=True):
     """
@@ -27,6 +26,36 @@ def canvas(with_attribution=True):
     if with_attribution:
         quote += "\n\t- Adapted from Henry David Thoreau"
     return quote
+
+
+def mean(num_list):
+    """
+    This function calculates the mean of a list.
+
+    Parameters
+    ----------
+    num_list : list
+        The list of numbers that we want to get the mean of.
+    
+    Returns
+    -------
+    mean_list : float
+        The mean of num_list.
+
+    Examples
+    --------
+    >>> mean([1,2,3,4,5])
+    3.0
+    """
+
+    if not isinstance(num_list, list):
+        raise TypeError("Mean: {} is not a list".format(num_list))
+
+    if len(num_list) == 0:
+        raise ZeroDivisionError("Mean: the input list is length zero")
+
+    mean_list = sum(num_list) / len(num_list)
+    return mean_list
 
 
 if __name__ == "__main__":
